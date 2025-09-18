c889409f668b4d92ecae1e99185dd6e8c0c1fec6
Duc-Vo-wq <votriduc0810@gmail.com>
2025-09-18 02:51:30 -0400

    update

--- js/script.js
+++ js/script.js
@@ -18,22 +18,10 @@ buttons[0].click();
 
 
 // Dark/Light Mode
-//Start
-const toggle = document.getElementById('theme-toggle');
-if(toggle){
-  const setTheme = theme => {
-    document.documentElement.setAttribute('data-theme', theme);
-    localStorage.setItem('theme', theme);
-  };
+const themeBtn = document.getElementById('theme-toggle');
+const root    = document.documentElement;      // refers to <html>
 
-// on click, swap light/dark
-  toggle.addEventListener('click', () => {
-    const current = document.documentElement.getAttribute('data-theme') || 'light';
-    setTheme(current === 'light' ? 'dark' : 'light');
-  });
-
-// initialize theme
-  const saved = localStorage.getItem('theme') || 'light';
-  setTheme(saved);
-}
-//End
+themeBtn.addEventListener('click', () => {
+  const nextTheme = root.dataset.theme === 'dark' ? 'light' : 'dark';
+  root.setAttribute('data-theme', nextTheme);
+});
